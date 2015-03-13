58369a790a4a8af25be98b78bdeb640cf4ad0bde
lijiao <lijiao@jd.com>
2015-03-13 05:37:03 -0400

    modify

--- library.sh
+++ library.sh
@@ -20,3 +20,46 @@ func_cur_time(){
 func_yesterday_time(){
     echo `date -d yesterday +"%Y-%m-%d %H:%M:%S"`
 }
+
+
+###############################################################################
+#                                                                             #
+#                                Color Operation                              #
+#                                                                             #
+###############################################################################
+
+#Input is the command.
+#The command's execute output will use red color
+func_red_cmd(){
+	echo  -n -e "\e[31m"
+	$*
+	echo  -n -e "\e[0m"
+}
+
+#Input is the command.
+#The command's execute output will use yellow color
+func_yellow_cmd(){
+	echo  -n -e "\e[33m"
+	$*
+	echo  -n -e "\e[0m"
+}
+
+#Input is a string.
+#The string  will be displayed with red color
+func_green_str(){
+	echo  -n -e "\e[32m"
+	echo "$*"
+	echo  -n -e "\e[0m"
+}
+
+#Input is the command
+#If command is error, display the error
+func_error_cmd(){
+	$*
+	ret=$?
+	if [ ! $ret -eq 0 ];then
+		echo  -n -e "\e[41;37m"
+		echo "Error: [$ret] $*"
+		echo  -n -e "\e[0m"
+	fi
+}
